ca0d606401fa77a47ba08ada6f1ba0189d182476
M. Gaarder <77402889+m-gaarder@users.noreply.github.com>
2022-01-09 19:00:18 -0500

    Create python_with_folium.py
    import folium and start map

---
+++ python_with_folium.py
@@ -0,0 +1,13 @@
+##folium stuffs
+import folium
+from folium import plugins
+import ipywidgets
+import geocoder
+import geopy
+import numpy as np
+import pandas as pd
+##import a dataset here: ie from vega_datasets import data as vds
+
+#basic map
+#zoom and pan
+folium.Map()
